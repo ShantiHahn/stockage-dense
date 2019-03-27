9d06cd6d5a0236df1dd4fcda71a41d59f973c048
Shanti-hn <hahn.shanti@gmail.com>
2019-03-27 15:44:38 +0100

    ajouts premiers fichiers au git

---
+++ src/dense.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include vector
+#include iostream
+
+int main(){
+
+
+}
---
+++ src/vecteur.cpp
@@ -0,0 +1,13 @@
+// Fichier src/vecteur.cpp
+#include<vector> // pour les std::vector
+
+Vecteur (){
+
+}; // constructeur vide
+Vecteur (int N){
+
+};
+Vecteur (const Vecteur &v){
+
+}; // constructeur par recopie
+
